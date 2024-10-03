9ab221869adeb2fae8884a957d4cfb82ab5b91a0
nikit34 <permikov134@mail.ru>
2024-10-03 22:40:58 +0100

    Fix Config for multiple jsons

--- configs/config.py
+++ configs/config.py
@@ -3,21 +3,25 @@ import os
 
 
 class Config:
-    _config_data = None
+    _config_cache = {}
 
     @classmethod
     def load_config(cls, file_name):
-        if cls._config_data is None:
+        if file_name not in cls._config_cache:
             current_dir = os.path.dirname(os.path.abspath(__file__))
             file_path = os.path.join(current_dir, file_name)
             with open(file_path, 'r') as file:
-                cls._config_data = json.load(file)
-        return cls._config_data
+                cls._config_cache[file_name] = json.load(file)
+        return cls._config_cache[file_name]
 
     @classmethod
     def get_package_name(cls):
-        return cls.load_config('desired_caps.json')['appium:appPackage']
+        return cls.load_config('desired_caps.json').get('appium:appPackage')
 
     @classmethod
     def get_desired_caps(cls):
         return cls.load_config('desired_caps.json')
+
+    @classmethod
+    def get_implicitly_timeout(cls):
+        return cls.load_config('waiting.json').get('implicitly_timeout')
--- tests/conftest.py
+++ tests/conftest.py
@@ -10,5 +10,7 @@ def driver(request):
     desired_caps = Config.get_desired_caps()
     capabilities_options = UiAutomator2Options().load_capabilities(desired_caps)
     driver = webdriver.Remote("http://localhost:4723", options=capabilities_options)
+    implicitly_timeout = Config.get_implicitly_timeout()
+    driver.implicitly_wait(implicitly_timeout)
     yield driver
     driver.quit()
