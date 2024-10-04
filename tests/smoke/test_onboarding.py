67d99d0c62b7865654cae062fb6999590aa90ec1
nikit34 <permikov134@mail.ru>
2024-10-04 03:34:15 +0100

    Apply SystemDialogBtnElement

--- tests/smoke/test_onboarding.py
+++ tests/smoke/test_onboarding.py
@@ -5,5 +5,5 @@ from src.app.elements.system_dialog_btn_element import SystemDialogBtnElement
 
 @mark.usefixtures('driver', 'appium_service')
 class TestOnboarding:
-    def test_first_action(self):
-        pass
+    def test_first_action(self, driver):
+        SystemDialogBtnElement(driver, 2).is_visible().click()
