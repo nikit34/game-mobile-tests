5b2e690860000a958a448cad9aa30c4b11673dc7
nikit34 <permikov134@mail.ru>
2024-10-05 14:16:51 +0100

    Add default_move_factor

--- src/actions_manager.py
+++ src/actions_manager.py
@@ -6,21 +6,22 @@ class ActionsManager:
         self.driver = driver
         self.window_size = self.driver.get_window_size()
         self.duration = ActionsConfig.get_duration()
+        self.default_move_factor = (0.2, 0.8)
 
     def tap_by_coordinates(self, x, y):
         return self.driver.tap([(x, y)], self.duration)
 
     def swipe_down(self):
         start_x = end_x = self.window_size['width'] // 2
-        start_y = self.window_size['height'] * 0.8
-        end_y = self.window_size['height'] * 0.2
+        start_y = self.window_size['height'] * self.default_move_factor[1]
+        end_y = self.window_size['height'] * self.default_move_factor[0]
 
         return self.driver.swipe(start_x, start_y, end_x, end_y, self.duration)
 
     def swipe_up(self):
         start_x = end_x = self.window_size['width'] // 2
-        start_y = self.window_size['height'] * 0.2
-        end_y = self.window_size['height'] * 0.8
+        start_y = self.window_size['height'] * self.default_move_factor[0]
+        end_y = self.window_size['height'] * self.default_move_factor[1]
 
         return self.driver.swipe(start_x, start_y, end_x, end_y, self.duration)
 
