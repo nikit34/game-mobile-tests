9c5de45c57155d2196b755dfd6936f93004936ba
nikit34 <permikov134@mail.ru>
2024-10-06 07:40:22 +0100

    Add game_center_component.py

---
+++ src/app/components/game_center_component.py
@@ -0,0 +1,10 @@
+from appium.webdriver.common.appiumby import AppiumBy
+
+from src.app.elements._base_selenium_element import BaseSeleniumElement
+
+
+class GameCenterComponent(BaseSeleniumElement):
+    CLOSE_BTN = (AppiumBy.ACCESSIBILITY_ID, "UIA.GameCenter.SignIn.cancelButton")
+
+    def __init__(self, driver, locator):
+        super().__init__(driver, locator)
