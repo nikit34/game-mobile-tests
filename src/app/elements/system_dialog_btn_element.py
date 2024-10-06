04dc594b4213729625b363072040b623c358c297
nikit34 <permikov134@mail.ru>
2024-10-06 09:17:35 +0100

    Add ios to SystemDialogBtnElement

--- src/app/elements/system_dialog_btn_element.py
+++ src/app/elements/system_dialog_btn_element.py
@@ -2,10 +2,15 @@ from appium.webdriver.common.appiumby import AppiumBy
 
 from configs.desired_caps_config import DesiredCapsConfig
 from src.app.elements._multiple_base_selenium_elements import BaseSeleniumElements
+from src.environment_manager import EnvironmentManager
 
 
 class SystemDialogBtnElement(BaseSeleniumElements):
-    BUTTON_LOCATOR = (AppiumBy.ID, DesiredCapsConfig.get_package_name() + ":id/system_dialog_button")
+    OK_BTN = EnvironmentManager.execute_platform_specific_callback(
+        lambda: (AppiumBy.ID, DesiredCapsConfig.get_package_name() + ":id/system_dialog_button"),
+        lambda: (AppiumBy.ACCESSIBILITY_ID, "OK")
+    )
 
-    def __init__(self, driver, index):
-        super().__init__(driver, self.BUTTON_LOCATOR, index)
+    def __init__(self, driver, index=0):
+        super().__init__(driver, self.OK_BTN, index)
+
