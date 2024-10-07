784d0b396b811a52679a4705c07dae4ef8b9fe92
nikit34 <permikov134@mail.ru>
2024-10-07 03:52:15 +0100

    Add method

--- src/screenshots_manager.py
+++ src/screenshots_manager.py
@@ -1,11 +1,15 @@
+from datetime import datetime
 import os
 
 
 class ScreenshotsManager:
     def __init__(self, driver):
         self.driver = driver
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    def take_screenshot(self, screenshot_name):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        file_path = os.path.join(current_dir, 'screenshots/', screenshot_name + '.png')
+    def save_screenshot(self, screenshot_name):
+        file_path = os.path.join(self.current_dir, 'screenshots/', screenshot_name + "-" + str(datetime.now()) + ".png")
         self.driver.save_screenshot(file_path)
+
+    def get_screenshot(self):
+        return self.driver.get_screenshot_as_base64()
