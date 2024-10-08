1a326db409f59b0acb767d3bdbd440a8fee442c8
nikit34 <permikov134@mail.ru>
2024-10-08 06:59:30 +0100

    Refactoring test_detection.py

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -6,16 +6,18 @@ from tests.test_base import TestBase
 
 
 class TestDetection(TestBase):
+    def setup_method(self):
+        self.original_img = Image(path_image="screenshots/test_detection.png", resize_image=True)
+
     @pytest.mark.parametrize("field_image_path, expected_clusters", [
         ("app/elements/img/empty_field_element_1.png", FieldComponent.COORDINATES_FIELD_1),
         ("app/elements/img/empty_field_element_2.png", FieldComponent.COORDINATES_FIELD_2),
         ("app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE)
     ])
     def test_count_clusters(self, field_image_path, expected_clusters):
-        original_img = Image(path_image="screenshots/test_detection.png", resize_image=True)
         empty_field_img = Image(path_image=field_image_path)
 
-        detected_clusters = self.image_detector.get_coordinates_objects(original_img, empty_field_img)
+        detected_clusters = self.image_detector.get_coordinates_objects(self.original_img, empty_field_img)
 
         assert len(detected_clusters) == len(expected_clusters), "Number of clusters does not match."
 
@@ -25,9 +27,8 @@ class TestDetection(TestBase):
         ("app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE)
     ])
     def test_cluster_within_bounds(self, field_image_path, expected_clusters):
-        original_img = Image(path_image="screenshots/test_detection.png", resize_image=True)
         empty_field_img = Image(path_image=field_image_path)
 
-        detected_clusters = self.image_detector.get_coordinates_objects(original_img, empty_field_img)
+        detected_clusters = self.image_detector.get_coordinates_objects(self.original_img, empty_field_img)
 
         self.check_clusters(detected_clusters, expected_clusters)
