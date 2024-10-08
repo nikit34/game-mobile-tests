18f128d7f87ca00c61cae9de52ff022116590c14
nikit34 <permikov134@mail.ru>
2024-10-08 14:29:04 +0100

    Adapt configs for multiple settings

--- configs/images_detector_config.py
+++ configs/images_detector_config.py
@@ -2,40 +2,37 @@ from configs._config import Config
 
 
 class ImagesDetectorConfig(Config):
-    CONFIG = 'json/images_detector.json'
+    FOLDER_CONFIG = 'json/images_detectors/'
 
-    @classmethod
-    def get_n_octave_layers(cls):
-        return cls.load_config(cls.CONFIG).get('n_octave_layers')
+    def __init__(self, name_target):
+        self.config = self.get_images_detector(name_target)
 
-    @classmethod
-    def get_contrast_threshold(cls):
-        return cls.load_config(cls.CONFIG).get('contrast_threshold')
+    def get_images_detector(self, name_target):
+        return self.load_config(self.FOLDER_CONFIG + name_target + ".json")
 
-    @classmethod
-    def get_eps(cls):
-        return cls.load_config(cls.CONFIG).get('eps')
+    def get_n_octave_layers(self):
+        return self.config.get('n_octave_layers')
 
-    @classmethod
-    def get_clahe_clip_limit(cls):
-        return cls.load_config(cls.CONFIG).get('clahe_clip_limit')
+    def get_contrast_threshold(self):
+        return self.config.get('contrast_threshold')
 
-    @classmethod
-    def get_clahe_grid_size(cls):
-        return cls.load_config(cls.CONFIG).get('clahe_grid_size')
+    def get_eps(self):
+        return self.config.get('eps')
 
-    @classmethod
-    def get_min_cluster_area(cls):
-        return cls.load_config(cls.CONFIG).get('min_cluster_area')
+    def get_clahe_clip_limit(self):
+        return self.config.get('clahe_clip_limit')
 
-    @classmethod
-    def get_min_samples(cls):
-        return cls.load_config(cls.CONFIG).get('min_samples')
+    def get_clahe_grid_size(self):
+        return self.config.get('clahe_grid_size')
 
-    @classmethod
-    def get_ransac(cls):
-        return cls.load_config(cls.CONFIG).get('ransac')
+    def get_min_cluster_area(self):
+        return self.config.get('min_cluster_area')
 
-    @classmethod
-    def get_ransac_threshold(cls):
-        return cls.load_config(cls.CONFIG).get('ransac_threshold')
+    def get_min_samples(self):
+        return self.config.get('min_samples')
+
+    def get_ransac(self):
+        return self.config.get('ransac')
+
+    def get_ransac_threshold(self):
+        return self.config.get('ransac_threshold')
