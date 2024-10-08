c15ab80322a50b6d0e63c33f5480adbe5652c7af
nikit34 <permikov134@mail.ru>
2024-10-08 20:10:44 +0100

    Fix tuner

--- tests/parameter_tuner/parameter_tuner.py
+++ tests/parameter_tuner/parameter_tuner.py
@@ -9,39 +9,49 @@ from tests.parameter_tuner.test_data import test_data
 
 
 class ParameterTuner:
-    def __init__(self, image_detector, param_grid):
-        self.image_detector = image_detector
+    def __init__(self, image_detector_class, param_grid):
+        self.image_detector_class = image_detector_class
         self.param_grid = param_grid
         self.best_params = None
-        self.best_error = float('inf')
+        self.best_total_error = float('inf')
 
-    def evaluate(self, original_img, template_img, expected_clusters, error_callback):
+    def evaluate(self, test_data, error_callback):
         for params in tqdm(ParameterGrid(self.param_grid)):
-            self.image_detector.n_octave_layers = params['n_octave_layers']
-            self.image_detector.contrast_threshold = params['contrast_threshold']
-            self.image_detector.eps = params['eps']
-            self.image_detector.clahe_clip_limit = params['clahe_clip_limit']
-            self.image_detector.clahe_grid_size = params['clahe_grid_size']
-            self.image_detector.min_cluster_area = params['min_cluster_area']
-            self.image_detector.min_samples = params['min_samples']
-            self.image_detector.ransac = params['ransac']
-            self.image_detector.ransac_threshold = params['ransac_threshold']
+            total_error = 0
 
-            detected_clusters = image_detector.get_coordinates_objects(original_img(), template_img())
+            for test_item in test_data:
+                image_detector = self.image_detector_class(test_item.get("target"))
 
-            error = error_callback(detected_clusters, expected_clusters)
-            if error < self.best_error:
-                self.best_error = error
+                image_detector.n_octave_layers = params['n_octave_layers']
+                image_detector.contrast_threshold = params['contrast_threshold']
+                image_detector.eps = params['eps']
+                image_detector.clahe_clip_limit = params['clahe_clip_limit']
+                image_detector.clahe_grid_size = params['clahe_grid_size']
+                image_detector.min_cluster_area = params['min_cluster_area']
+                image_detector.min_samples = params['min_samples']
+                image_detector.ransac = params['ransac']
+                image_detector.ransac_threshold = params['ransac_threshold']
+
+                original_img = Image(path_image=test_item.get("original_img"))
+                template_img = Image(path_image=test_item.get("template_img"))
+
+                detected_clusters = image_detector.get_coordinates_objects(original_img, template_img)
+
+                error = error_callback(detected_clusters, test_item.get("expected_clusters"))
+                total_error += error
+
+            if total_error < self.best_total_error:
+                self.best_total_error = total_error
                 self.best_params = params
 
-            if not error:
+            if not total_error:
                 break
 
-        return self.best_params, self.best_error
+        return self.best_params, self.best_total_error
 
 
 if __name__ == "__main__":
-    NEED_TEST_DATA_INDEXES = [0]
+    NEED_TEST_DATA_INDEXES = [0, 1, 2]
     NEED_ERROR_CALLBACK = feedback_count_clusters
     param_grid = {
         "n_octave_layers": [3, 5, 15, 50],
@@ -58,15 +68,7 @@ if __name__ == "__main__":
     images_manager = ImagesManager()
     images_manager.remove("temporary_images")
 
+    tuner = ParameterTuner(ImageDetector, param_grid)
     selected_test_data = [test_data[i] for i in NEED_TEST_DATA_INDEXES]
-    for test_item in selected_test_data:
-        image_detector = ImageDetector(test_item.get("target"))
-        tuner = ParameterTuner(image_detector, param_grid)
-        best_params, best_error = tuner.evaluate(
-            original_img=lambda: Image(path_image=test_item.get("original_img")),
-            template_img=lambda: Image(path_image=test_item.get("template_img")),
-            expected_clusters=test_item.get("expected_clusters"),
-            error_callback=NEED_ERROR_CALLBACK
-        )
-
-        print("Best params: \n" + str(best_params) + "\n with error: " + str(best_error) + " for test data: " + str(test_item))
+    best_params, best_total_error = tuner.evaluate(selected_test_data, NEED_ERROR_CALLBACK)
+    print("Best overall params: \n" + str(best_params) + "\n with total error: " + str(best_total_error))
