f6158d89a0952e31132f741b37b2b63aa8e7b8f6
nikit34 <permikov134@mail.ru>
2024-10-08 08:26:02 +0100

    Add test data

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -6,27 +6,32 @@ from tests.test_base import TestBase
 
 
 class TestDetection(TestBase):
-    def setup_method(self):
-        self.original_img = Image(path_image="screenshots/test_detection.png", resize_image=True)
-
-    @pytest.mark.parametrize("field_image_path, expected_clusters", [
-        ("app/elements/img/empty_field_element_1.png", FieldComponent.COORDINATES_FIELD_1),
-        ("app/elements/img/empty_field_element_2.png", FieldComponent.COORDINATES_FIELD_2),
-        ("app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE)
+    @pytest.mark.parametrize("original_image_path, field_image_path, expected_clusters", [
+        ("screenshots/test_detection_1.png", "app/elements/img/empty_field_element_1.png", FieldComponent.COORDINATES_FIELD_1),
+        ("screenshots/test_detection_1.png", "app/elements/img/empty_field_element_2.png", FieldComponent.COORDINATES_FIELD_2),
+        ("screenshots/test_detection_1.png", "app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE),
+        ("screenshots/test_detection_2.png", "app/elements/img/empty_field_element_1.png", FieldComponent.COORDINATES_FIELD_1),
+        ("screenshots/test_detection_2.png", "app/elements/img/empty_field_element_2.png", FieldComponent.COORDINATES_FIELD_2),
+        ("screenshots/test_detection_2.png", "app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE)
     ])
-    def test_count_clusters(self, field_image_path, expected_clusters):
+    def test_count_clusters(self, original_image_path, field_image_path, expected_clusters):
+        self.original_img = Image(path_image=original_image_path, resize_image=True)
         empty_field_img = Image(path_image=field_image_path)
 
         detected_clusters = self.image_detector.get_coordinates_objects(self.original_img, empty_field_img)
 
         assert len(detected_clusters) == len(expected_clusters), "Number of clusters does not match."
 
-    @pytest.mark.parametrize("field_image_path, expected_clusters", [
-        ("app/elements/img/empty_field_element_1.png", FieldComponent.COORDINATES_FIELD_1),
-        ("app/elements/img/empty_field_element_2.png", FieldComponent.COORDINATES_FIELD_2),
-        ("app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE)
+    @pytest.mark.parametrize("original_image_path, field_image_path, expected_clusters", [
+        ("screenshots/test_detection_1.png", "app/elements/img/empty_field_element_1.png", FieldComponent.COORDINATES_FIELD_1),
+        ("screenshots/test_detection_1.png", "app/elements/img/empty_field_element_2.png", FieldComponent.COORDINATES_FIELD_2),
+        ("screenshots/test_detection_1.png", "app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE),
+        ("screenshots/test_detection_2.png", "app/elements/img/empty_field_element_1.png", FieldComponent.COORDINATES_FIELD_1),
+        ("screenshots/test_detection_2.png", "app/elements/img/empty_field_element_2.png", FieldComponent.COORDINATES_FIELD_2),
+        ("screenshots/test_detection_2.png", "app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE)
     ])
-    def test_cluster_within_bounds(self, field_image_path, expected_clusters):
+    def test_cluster_within_bounds(self, original_image_path, field_image_path, expected_clusters):
+        self.original_img = Image(path_image=original_image_path, resize_image=True)
         empty_field_img = Image(path_image=field_image_path)
 
         detected_clusters = self.image_detector.get_coordinates_objects(self.original_img, empty_field_img)
