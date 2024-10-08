b5a514c06b344cd1b7f61d72148d8398a8426197
nikit34 <permikov134@mail.ru>
2024-10-08 01:12:22 +0100

    v1

--- src/image_detector.py
+++ src/image_detector.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from sklearn.cluster import DBSCAN
+
 from configs.images_detector_config import ImagesDetectorConfig
 
 
@@ -11,35 +12,17 @@ class ImageDetector:
             clahe_clip_limit=ImagesDetectorConfig.get_clahe_clip_limit(),
             clahe_grid_size=ImagesDetectorConfig.get_clahe_grid_size(),
             min_color_similarity=ImagesDetectorConfig.get_min_color_similarity(),
-            min_cluster_area=ImagesDetectorConfig.get_min_cluster_area(),
-            min_samples=ImagesDetectorConfig.get_min_samples(),
-            flann_trees=5,
-            flann_checks=50,
-            good_match_threshold=0.75,
-            ransac_threshold=5.0
+            min_cluster_area=ImagesDetectorConfig.get_min_cluster_area()
     ):
         self.eps = eps
         self.clahe_clip_limit = clahe_clip_limit
         self.clahe_grid_size = clahe_grid_size
         self.min_color_similarity = min_color_similarity
         self.min_cluster_area = min_cluster_area
-        self.min_samples = min_samples
-        self.flann_trees = flann_trees
-        self.flann_checks = flann_checks
-        self.good_match_threshold = good_match_threshold
-        self.ransac_threshold = ransac_threshold
 
     def apply_clahe(self, image):
         clahe = cv2.createCLAHE(clipLimit=self.clahe_clip_limit, tileGridSize=self.clahe_grid_size)
         return clahe.apply(image)
-
-    def increase_contrast(self, image):
-        lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-        l, a, b = cv2.split(lab)
-        clahe = cv2.createCLAHE(clipLimit=self.clahe_clip_limit, tileGridSize=self.clahe_grid_size)
-        l = clahe.apply(l)
-        lab = cv2.merge((l, a, b))
-        return cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
 
     @staticmethod
     def get_color_histogram(image, bins=(8, 8, 8)):
@@ -50,25 +33,13 @@ class ImageDetector:
     @staticmethod
     def compute_sift_keypoints_and_descriptors(gray_image):
         sift = cv2.SIFT_create()
-        keypoints, descriptors = sift.detectAndCompute(gray_image, None)
-        if descriptors is None:
-            return [], None
-        return keypoints, descriptors
+        return sift.detectAndCompute(gray_image, None)
 
-    def match_descriptors(self, des1, des2):
-        if des1 is None or des2 is None:
-            return []
-        FLANN_INDEX_KDTREE = 1
-        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=self.flann_trees)
-        search_params = dict(checks=self.flann_checks)
-        flann = cv2.FlannBasedMatcher(index_params, search_params)
-        matches = flann.knnMatch(des1, des2, k=2)
-
-        good_matches = []
-        for m, n in matches:
-            if m.distance < self.good_match_threshold * n.distance:
-                good_matches.append(m)
-        return good_matches
+    @staticmethod
+    def match_descriptors(des1, des2):
+        bf = cv2.BFMatcher(cv2.NORM_L2)
+        matches = bf.match(des1, des2)
+        return sorted(matches, key=lambda x: x.distance)
 
     @staticmethod
     def extract_coordinates_from_matches(matches, keypoints):
@@ -81,11 +52,12 @@ class ImageDetector:
     def perform_clustering(self, coordinates):
         if len(coordinates) > 0:
             coordinates = np.array(coordinates)
-            clustering = DBSCAN(eps=self.eps, min_samples=self.min_samples).fit(coordinates)
+            clustering = DBSCAN(eps=self.eps).fit(coordinates)
             cluster_labels = clustering.labels_
-            return cluster_labels
+            clustered_coords = coordinates[cluster_labels != -1]
+            return clustered_coords, cluster_labels
         else:
-            return []
+            return np.array([]), []
 
     def get_cluster_bounds(self, coordinates, cluster_labels):
         cluster_bounds = []
@@ -107,22 +79,15 @@ class ImageDetector:
         return cluster_bounds
 
     @staticmethod
-    def draw_clusters_and_points(image, cluster_bounds, coordinates, cluster_labels):
-        coordinates = np.array(coordinates)
-
+    def draw_clusters_and_points(image, cluster_bounds, clustered_coords):
         for i, ((x_min, y_min), (x_max, y_max)) in enumerate(cluster_bounds, start=1):
-            cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)
+            cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
 
             label_position = (x_min, y_min - 10) if y_min - 10 > 0 else (x_min, y_min + 10)
-            cv2.putText(image, str(i), label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            cv2.putText(image, str(i), label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-        clustered_coords = coordinates[cluster_labels != -1]
         for x, y in clustered_coords:
-            cv2.circle(image, (x, y), 3, (0, 255, 0), -1)
-
-        noise_coords = coordinates[cluster_labels == -1]
-        for x, y in noise_coords:
-            cv2.circle(image, (x, y), 3, (0, 0, 255), -1)
+            cv2.circle(image, (x, y), 5, (255, 0, 0), -1)
 
     @staticmethod
     def convert_to_color_if_needed(image):
@@ -130,25 +95,9 @@ class ImageDetector:
             return cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
         return image
 
-    def apply_ransac(self, kp1, kp2, matches):
-        if len(matches) < 4:
-            return None, []
-
-        src_pts = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-        dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
-
-        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, self.ransac_threshold)
-        matchesMask = mask.ravel().tolist()
-
-        good_matches = [m for i, m in enumerate(matches) if matchesMask[i] == 1]
-        return M, good_matches
-
     def get_coordinates_objects(self, original_img, template_img):
         original_img.cv_image = self.convert_to_color_if_needed(original_img.cv_image)
         template_img.cv_image = self.convert_to_color_if_needed(template_img.cv_image)
-
-        original_img.cv_image = self.increase_contrast(original_img.cv_image)
-        template_img.cv_image = self.increase_contrast(template_img.cv_image)
 
         gray_original = cv2.cvtColor(original_img.cv_image, cv2.COLOR_BGR2GRAY)
         gray_template = cv2.cvtColor(template_img.cv_image, cv2.COLOR_BGR2GRAY)
@@ -160,12 +109,12 @@ class ImageDetector:
         kp2, des2 = self.compute_sift_keypoints_and_descriptors(gray_original)
 
         matches = self.match_descriptors(des1, des2)
-
         coordinates = self.extract_coordinates_from_matches(matches, kp2)
-        cluster_labels = self.perform_clustering(coordinates)
+
+        clustered_coords, cluster_labels = self.perform_clustering(coordinates)
         cluster_bounds = self.get_cluster_bounds(coordinates, cluster_labels)
 
-        self.draw_clusters_and_points(original_img.cv_image, cluster_bounds, coordinates, cluster_labels)
+        self.draw_clusters_and_points(original_img.cv_image, cluster_bounds, clustered_coords)
 
         color_hist_template = self.get_color_histogram(template_img.cv_image)
         color_hist_original = self.get_color_histogram(original_img.cv_image)
