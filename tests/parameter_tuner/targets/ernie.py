f2a811fc4d3f6ce948fc214e2d01365269913ae0
nikit34 <permikov134@mail.ru>
2024-10-09 23:59:11 +0100

    Fix ernie

--- tests/parameter_tuner/targets/ernie.py
+++ tests/parameter_tuner/targets/ernie.py
@@ -7,11 +7,6 @@ class ErnieTarget:
 
     TEST_DATA = [
         {
-            "original_img": "screenshots/test_detection_0.png",
-            "template_img": "app/elements/img/ernie_element.png",
-            "expected_clusters": ErnieElement.COORDINATES_ERNIE
-        },
-        {
             "original_img": "screenshots/test_detection_1.png",
             "template_img": "app/elements/img/ernie_element.png",
             "expected_clusters": ErnieElement.COORDINATES_ERNIE
@@ -42,13 +37,13 @@ class ErnieTarget:
     ERROR_CALLBACK = feedback_cluster_within_bounds
 
     PARAM_GRID = {
-        "clahe_clip_limit": [2.0, 3.0],
-        "clahe_grid_size": [[1, 1], [5, 1]],
-        "contrast_threshold": [0.01, 0.02],
-        "eps": [30, 50],
-        "min_cluster_area": [1000],
-        "min_samples": [1, 3],
-        "n_octave_layers": [2, 50],
-        "ransac": [True, False],
-        "ransac_threshold": [3, 5]
+        "clahe_clip_limit": [1.0, 2.0],
+        "clahe_grid_size": [[1, 1]],
+        "contrast_threshold": [0.01],
+        "eps": [30, 40],
+        "min_cluster_area": [600],
+        "min_samples": [1],
+        "n_octave_layers": [2, 3],
+        "ransac": [True],
+        "ransac_threshold": [3, 4]
     }
