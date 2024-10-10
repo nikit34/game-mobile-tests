f9a9d3919b673fabf9d21acc7627fd9a91bf4170
nikit34 <permikov134@mail.ru>
2024-10-10 06:54:21 +0100

    save sceenshots in another folder

--- src/image/screenshot.py
+++ src/image/screenshot.py
@@ -10,7 +10,7 @@ class Screenshot(FileManager):
         super().__init__()
 
     def save(self, screenshot_name):
-        file_path = os.path.join(self.current_dir, 'screenshots/', screenshot_name + "-" + str(datetime.now()) + ".png")
+        file_path = os.path.join(self.current_dir, "image", "tmp_screenshots", screenshot_name + "-" + str(datetime.now()) + ".png")
         self.driver.save_screenshot(file_path)
 
     def get_screenshot(self):
