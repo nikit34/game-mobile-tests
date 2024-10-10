0b9c19de6b9c18d33b1ba84b04bb06bc056bb595
nikit34 <permikov134@mail.ru>
2024-10-10 15:42:27 +0100

    Add wheat to test data

--- tests/test_data/detection.py
+++ tests/test_data/detection.py
@@ -1,6 +1,6 @@
 from src.app.components.field_component import FieldComponent
 from src.app.elements.ernie_element import ErnieElement
-
+from src.app.elements.wheat_element import WheatElement
 
 TEST_DATA = [
     ("empty_field", "app/screens/polygons/empty_fields_ernie_0.png", "app/elements/img/empty_field_element_1.png", FieldComponent.COORDINATES_FIELD_1),
@@ -17,5 +17,6 @@ TEST_DATA = [
     ("ernie", "app/screens/polygons/empty_fields_ernie_1.png", "app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE),
     ("ernie", "app/screens/polygons/empty_fields_ernie_2.png", "app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE),
     ("ernie", "app/screens/polygons/empty_fields_ernie_3.png", "app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE),
-    ("ernie", "app/screens/polygons/empty_fields_ernie_4.png", "app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE)
+    ("ernie", "app/screens/polygons/empty_fields_ernie_4.png", "app/elements/img/ernie_element.png", ErnieElement.COORDINATES_ERNIE),
+    ("wheat", "app/screens/polygons/wheat_0.png", "app/elements/img/wheat_element.png", WheatElement.COORDINATES_WHEAT),
 ]
