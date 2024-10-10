0012147fff3c12a5b063392a8ffd2218e87edbee
nikit34 <permikov134@mail.ru>
2024-10-10 15:03:27 +0100

    Check coords for wheat

---
+++ src/app/elements/wheat_element.py
@@ -0,0 +1,10 @@
+from src.app.elements._base_elements._base_opencv_element import BaseOpencvElement
+
+
+class WheatElement(BaseOpencvElement):
+    COORDINATES_WHEAT = (
+        ((425, 325), (475, 390)),
+    )
+
+    def __init__(self, driver):
+        super().__init__(driver)
