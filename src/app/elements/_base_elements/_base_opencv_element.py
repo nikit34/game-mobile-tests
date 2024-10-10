45710686bb7d2a23394e591a3e4db0c6ab6dc89e
nikit34 <permikov134@mail.ru>
2024-10-10 05:55:28 +0100

    Add _parse_coordinates to BaseOpencvElement

--- src/app/elements/_base_elements/_base_opencv_element.py
+++ src/app/elements/_base_elements/_base_opencv_element.py
@@ -11,8 +11,20 @@ class BaseOpencvElement:
         center_y = (y1 + y2) / 2
         return center_x, center_y
 
+    @staticmethod
+    def _parse_coordinates(coordinates):
+        if len(coordinates) == 2:
+            (x1, y1), (x2, y2) = coordinates
+        elif len(coordinates) == 1:
+            (x1, y1), (x2, y2) = coordinates[0]
+        elif len(coordinates) == 4:
+            x1, y1, x2, y2 = coordinates
+        else:
+            raise ValueError("Incorrect coordinate format")
+        return x1, y1, x2, y2
+
     def click(self, coordinates):
-        (x1, y1), (x2, y2) = coordinates
+        x1, y1, x2, y2 = self._parse_coordinates(coordinates)
         actions = ActionsManager(self.driver)
         x, y = self.get_center_coordinates(x1, y1, x2, y2)
         actions.tap_by_coordinates(x, y)
