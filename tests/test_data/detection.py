bf953f96b5a0b7573660abd6d2629fda533e5b53
nikit34 <permikov134@mail.ru>
2024-10-11 08:28:05 +0100

    Add tests data to test

--- tests/test_data/detection.py
+++ tests/test_data/detection.py
@@ -19,4 +19,6 @@ TEST_DATA = [
     ("ernie", "app/screens/polygons/img/empty_fields_ernie_3.png", "app/elements/img/ernie.png", ErnieElement.COORDINATES_ERNIE),
     ("ernie", "app/screens/polygons/img/empty_fields_ernie_4.png", "app/elements/img/ernie.png", ErnieElement.COORDINATES_ERNIE),
     ("wheat", "app/screens/polygons/img/wheat_0.png", "app/elements/img/wheat.png", WheatElement.COORDINATES_WHEAT),
+    ("wheat_field", "app/screens/polygons/img/wheat_fields_0.png", "app/screens/polygons/components/field/elements/img/wheat_field_1.png", FieldComponent.COORDINATES_FIELD_1),
+    ("wheat_field", "app/screens/polygons/img/wheat_fields_0.png", "app/screens/polygons/components/field/elements/img/wheat_field_2.png", FieldComponent.COORDINATES_FIELD_1),
 ]
